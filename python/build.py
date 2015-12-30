251f750804a98461608a4fac234c965cd8a377ee
罗豪 <lh@hzjs.co>
2015-12-30 16:14:38 +0800

    temp

--- python/build.py
+++ python/build.py
@@ -3,3 +3,26 @@
 
 # 读取文本文件生成指定格式的json文件
 
+import sys
+import os
+
+class crac:
+    def __init__(self):
+        pass
+    def init(self):
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+    def readFile(self, path):
+        f = open(path, 'rb')
+        lines = f.readlines()
+        print lines[111].strip()
+    def run(self):
+        print "run"
+
+def main():
+    c = crac()
+    c.init()
+    c.readFile(u'../files/总题库文件(v150612).txt')
+
+if __name__=='__main__':
+    main()
